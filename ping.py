1051fe4591fe2adac29af408c534eca95bcd9f07
Sergio <sergino_89@yahoo.it>
2017-08-04 04:33:34 -0400

    Correct versione pushed on 04-08-2017
     Your branch is up-to-date with 'origin/addLog'.
    
     Changes to be committed:
    	modified:   log.out
    	modified:   ping.py
    	modified:   serverstatus
    	new file:   serverstatus.original

--- ping.py
+++ ping.py
@@ -10,7 +10,7 @@ from threading import Thread
 
 
 #LOGGER
-LOG_FILENAME = 'log.out'
+LOG_FILENAME = '/home/sergio/Documents/statistics/serverUptime/log.out'
 logging.basicConfig(filename=LOG_FILENAME,
                     level=logging.DEBUG,
                     )
@@ -28,6 +28,8 @@ crash = 0
 i = 0
 frequencyChekServerStatus = 50 #in seconds
 
+initializeUptime = 0
+initializeDowntime = 0
 
 
 #DATABASE
@@ -73,6 +75,16 @@ def developer():
 	global downtime
 	global isUp
 	global crash
+	global initializeUptime
+
+
+	#initializa array
+#	for i in range(3):
+#		uptime[i]=initializeUptime
+#		downtime[i]=initializeUptime
+#		initializeUptime = initializeUptime + 1
+
+	initializeUptime = 0
 	
 
 	if crash == 0: 
@@ -81,15 +93,17 @@ def developer():
 				crash = 1
 				cur.execute('SELECT uptime FROM statistiche WHERE ip= "' + key + '";')
 				result  = cur.fetchone()
-				logging.debug('DEVELOPER - uptime from database: '+ str(result)+' for ip: '+str(key))
 				uptime.append(int(result[0]))
-
+		
 				cur.execute('SELECT downtime FROM statistiche WHERE ip="' + key + '";')
 				result  = cur.fetchone()
-				logging.debug('DEVELOPER - downtime from database: '+str(result)+' for ip: '+str(key))
 				downtime.append(int(result[0]))
+				logging.debug('DEVELOPER - downtime: '+str(downtime[0]))
+
 			except Exception:
 				print "IP: "+str(key)+" not found in table statistics"
+				print(traceback.format_exc())
+				logging.debug("IP: "+str(key)+" not found in table statistics")
 
 
 
@@ -101,6 +115,7 @@ def developer3loop(uptime, downtime):
 	global crash
 	global i
 	global frequencyChekServerStatus
+	global initializeUptime
 
 	db3 = sqlite3.connect('/home/sergio/Documents/statistics/serverUptime/serverstatus')
 	#db3 = pymysql.connect(host="localhost",    	# your host, usually localhost
@@ -113,6 +128,7 @@ def developer3loop(uptime, downtime):
 	
 	
 	while True:
+		initializeUptime = 0
 		for key, value in serverList.items():
 			developer3 = str(key)
 			
@@ -121,21 +137,37 @@ def developer3loop(uptime, downtime):
 
 				if response3 == 0:
 					isUp.insert(i, 1)
-					logging.debug("i vale: "+str(i));
+					print("IF - i vale: "+str(i));
+
+					for k in range(3):
+						print ('uptime '+str(k)+ ' '+str(uptime[k]))
+
 					uptimeTmp = uptime[i]
+					logging.debug('IF - uptime['+str(i)+']= '+str(uptime[i]))
 					uptime.insert(i, uptimeTmp+frequencyChekServerStatus)
-					logging.debug('DEVELOPER3LOOP - uptime for ip '+ str(key) + ' : ' + str(uptimeTmp))
+					print('uptime '+str(i)+ ' ' + str(uptime[i]))
+					print('DEVELOPER3LOOP - uptime for ip '+ str(key) + ' : ' + str(uptimeTmp))
 					sqlInsert3 = 'UPDATE `statistiche` SET `uptime` = "' + str(uptime[i]) +'" WHERE `statistiche`.`ip` = "' + str(key) + '";'
+					print('QUERY IF: '+sqlInsert3)
 					cur3.execute(sqlInsert3)
 					db3.commit()
 					uptimeTmp = 0
 
 				else:
 					isUp.insert(i, 0)
+					print("ELSE - i vale: "+str(i));
 					downtimeTmp = downtime[i]
+
+                                        for l in range(3):
+                                                print ('downtime '+str(l)+ ' '+str(downtime[l]))
+
+
+					logging.debug('IF - downtime['+str(i)+']= '+str(downtime[i]))
 					downtime.insert(i, downtimeTmp+frequencyChekServerStatus)
-					logging.debug('DEVELOPER3LOOP - downtime for ip '+ str(key) + ' : ' + str(downtimeTmp))
+					print('downtime '+str(i)+ ' ' + str(downtime[i]))
+					print('DEVELOPER3LOOP - downtime for ip '+ str(key) + ' : ' + str(downtimeTmp))
 					sqlInsert2 = 'UPDATE `statistiche` SET `downtime` = "' + str(downtime[i]) +'" WHERE `statistiche`.`ip` = "' + str(key) + '";'
+					print('QUERY ELSE: '+sqlInsert2)
 					cur3.execute(sqlInsert2)
 					db3.commit()
 					downtimeTmp = 0
@@ -147,7 +179,11 @@ def developer3loop(uptime, downtime):
 
 			i = i+1	
 			time.sleep(frequencyChekServerStatus)
+			print('finito for '+str(i))
+		
+		
 		i=0	
+		print('whilee '+str(i))
  		time.sleep (5)
 
 initializeDB()		
